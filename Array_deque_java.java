eaa74801b4ca12058c790cf505309ce0c7d81389
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-09-30 18:51:34 +0530

    Add files via upload

---
+++ Array_deque_java.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Array_deque_java {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        ad1.add(6);
+        ad1.add(56);
+        ad1.add(9);
+        ad1.addFirst(5);
+        System.out.println(ad1.getFirst());
+        System.out.println(ad1.getLast());
+    }
+}
