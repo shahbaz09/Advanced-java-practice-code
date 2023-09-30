cf6d86214de02673eaa05f044769d1f3b967bbed
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-09-30 18:52:40 +0530

    Add files via upload

---
+++ Hashset.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> myHashSet = new HashSet<>(6, 0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(3);
+        myHashSet.add(11);
+        myHashSet.add(11);
+        System.out.println(myHashSet);
+    }
+}
