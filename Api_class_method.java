6bb2ae368b833d7be81f695a3883c32f4f26bbed
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-10-01 04:04:04 +0530

    Add files via upload

---
+++ Api_class_method.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Api_class_method {
+    public static void main(String[] args) {
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
