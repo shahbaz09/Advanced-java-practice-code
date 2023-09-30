5872f575d14bfdae16b81642a48e65fd4878d86c
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-10-01 04:02:33 +0530

    Add files via upload

---
+++ Calendar_class.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Calendar_class{
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(" The maximum no. of weeks in a year : " + c.getMaximum(Calendar.WEEK_OF_YEAR));
+
+
+    }
+}
