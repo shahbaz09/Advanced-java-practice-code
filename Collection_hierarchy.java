aa7bff558ac0f592043580232419ac2d27e983a9
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-09-30 18:50:01 +0530

    Add files via upload

---
+++ Collection_hierarchy.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Collection_hierarchy {
+    public static void main(String[] args) {
+        // ArrayList
+        // Set
+        // TreeSet
+    }
+}
